02bc707737014695c6023e3da75efe0a93a44208
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-06-17 11:38:30 +0100

    connected with api

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Cats from './components/Cats/Cats';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Cats />
     </div>
   );
 }
---
+++ client/src/components/Cats/Cats.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Cats.scss'
+
+
+const Cats = () => {
+    const [cats, setCats] = useState([]);
+  useEffect(() => {
+    // connect data
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/v1/breeds");
+      setCats(res.data);
+      console.log(res.data)
+      } catch (error) {
+        console.error(error)
+      }
+      
+    };
+    fetchData();
+  }, []);
+  return (
+    <div></div>
+  )
+}
+
+export default Cats
