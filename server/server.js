986d677d94484f7f3f512d3c9c57482ffbb41139
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-06-17 10:58:50 +0100

    server side setup , depencies installed

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require('cors')
+
+//Set up port
+const PORT = 5000;
+
+const app = express();
+
+// Cross-origin requests
+app.use(cors({
+    origin: "*",
+  }));
+// Get data from cat api
+app.get("/v1/breeds", async (req, res) => {
+  try {
+    await axios
+      .get('https://api.thecatapi.com/v1/breeds')
+      .then((response) => {
+        res.send(response.data);
+        console.log(response.data.length)
+      });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// run server on port 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
